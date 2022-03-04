87853e361fa8896dac5fdb715c6fb9a400a4d899
kyu05017 <kyu05017@kyu05017-Surfice>
2022-03-05 08:46:44 +0900

    dd

--- kks/src/Day04/Day04_5.java
+++ kks/src/Day04/Day04_5.java
@@ -16,8 +16,39 @@ public class Day04_5 {
 				//2. 중복 x
 			//3사용자 추첨번호와 컴ㅍ퓨터 추첨번호 동일 수의 개수
 		
-
+		Scanner scanner = new Scanner(System.in);
 		
+		//준비
+		int[] myNumber = new int[6];	//사용자가 선택한 변호 목록
+		int[] lottoNumber = new int[6]; //컴퓨터의 난수 번호 목록
+		
+		for(int i = 0; i < 6; i++) {
+			//i 는 01nxj 6미만까지 1씩 증가하면서 6번 반복
+			System.out.println("1~45 사이 숫자 입력 : ");
+			int inputNumber = scanner.nextInt();
+			
+			boolean pass = true; //저장되는 조건 변수 
+			
+			//유효성 검사
+			if(inputNumber < 1 || inputNumber > 45) { 
+				//로또 번호인 1부터 45중 
+				//내가 쓴 숫자가 1보다 작거나 || 45보다 크면
+				//if 조건문 실행
+				System.out.println("사용할수 없는 숫자입니다.");
+				i--; // 반복횟수 초기화;
+				pass = false; // 통과변수를 저장하지 않음
+			}
+			//중복검사
+			for(int j = 0 ; j < 6; j++) {
+				if(inputNumber == myNumber[j]) {
+					System.out.println("중복된 숫자 입니다.");
+					i--;
+					pass = false;
+				}
+			}
+			if( pass == true) myNumber[i] = inputNumber;
+			
+		}
 		
 
 	}
