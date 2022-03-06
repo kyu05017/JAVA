20c8aa1f138a05625874f8daf2d3194c2a6983eb
kyu05 <kyu05@121.139.36.131>
2022-03-07 06:35:58 +0900

    dd

---
+++ kks/src/baekjoon/Hello_World.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,world");
+
+	}
+
+}
---
+++ kks/src/baekjoon/강한육군.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class 강한육군 {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+
+	}
+
+}
