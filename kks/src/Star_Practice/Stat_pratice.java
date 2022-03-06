02c94919580f306ec2a503b020047340d8f08bac
kyu05 <kyu05@121.139.36.131>
2022-03-06 13:54:49 +0900

    ff

--- kks/src/Star_Practice/Stat_pratice.java
+++ kks/src/Star_Practice/Stat_pratice.java
@@ -3,8 +3,25 @@ package Star_Practice;
 public class Stat_pratice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * 
+		 * 별문제11 : 입력받은 줄만큼 출력 [ 입력받은 행/열 동일 ] 
+		  
+		  
+		 	*     *		
+		 	 *   *	
+		 	  * *		
+		 	   *		
+		 	  * *		
+		 	 *   *		
+		 	*     *		
+			
+		 */
+		
+		
+		for(int i = 1; i <= 7; i++) {
+			System.out.print("*");
+		}
 	}
 
 }
