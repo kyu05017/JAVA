4fa65399141158f4432b6a1c52bbac23347b6348
504 <504@DESKTOP-M91NLNL>
2022-03-07 09:31:42 +0900

    백준연습

---
+++ kks/src/baekjoon/점수입력.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class 점수입력 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int grade = scanner.nextInt();
+		
+		if(grade >= 90) {
+			System.out.println("A");
+		}
+		else if(grade >= 80) {
+			System.out.println("B");
+		}
+		else if(grade >= 70) {
+			System.out.println("C");
+		}
+		else if(grade >= 60) {
+			System.out.println("D");
+		}
+		else {
+			System.out.println("F");
+		}
+	
+	}
+
+}
