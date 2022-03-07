0952f2869e8b3deaf824e97a2e5ffb93f6e5b655
504 <504@DESKTOP-M91NLNL>
2022-03-07 14:25:05 +0900

    ㅇㅇ

---
+++ kks/src/Day05/Day05_4_과제.java
@@ -0,0 +1,15 @@
+package Day05;
+
+public class Day05_4_과제 {
+	/*	1.입출력 2.변수/자료형/연산 3.제어/반복문 4.제어/반복문 5.배열
+	 * 
+	 *  도서 대여 console 프로그램
+	 * 	회원제
+	 * 
+	 * 
+	 * */
+	public static void main(String[] args) {
+		
+	}
+
+}
