49ecc1949c30ef96fc3c8e1cd0f425fedba2f061
504 <504@DESKTOP-M91NLNL>
2022-03-07 09:45:22 +0900

    5일ㅏ

--- kks/src/Day05/Day05_1.java
+++ kks/src/Day05/Day05_1.java
@@ -1,10 +1,14 @@
 package Day05;
 
-public class Day05_1 {
+public class Day05_1 {//c s
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	public static void main(String[] args) {//m s
+		
+		// 변수 : 하나의 데이터 저장
+		// 배열 : 동일한 자료형 데이터를 특정기준 [ 인덱스 ]으로 나열
+		// 1차원 배열 :  
+		// 2차월 배열 :
+		// 
+		
+	}//m e
+}//s e
--- kks/src/baekjoon/윤년구하기.java
+++ kks/src/baekjoon/윤년구하기.java
@@ -8,14 +8,13 @@ public class À±³â±¸ÇÏ±â {
 		Scanner scanner = new Scanner(System.in);
 		
 		int a = scanner.nextInt();
-		
-		if((a%4)==0  &&(a%100)!=0 && (a%400)==0){
-			System.out.println("1");
+		if(a >= 1 && a <= 4000) {
+			if((a%4)==0  &&(a%100)!=0 && (a%400)==0){
+				System.out.println("1");
+			}
+			else {
+				System.out.println("0");
+			}
 		}
-		else {
-			System.out.println("0");
-		}
-
 	}
-
 }
