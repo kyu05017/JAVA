196b490b9f4adab2d73feda3ffe6c49c0dddba9f
504 <504@DESKTOP-M91NLNL>
2022-03-07 10:35:31 +0900

    dd

--- kks/src/baekjoon/윤년구하기.java
+++ kks/src/baekjoon/윤년구하기.java
@@ -8,8 +8,9 @@ public class À±³â±¸ÇÏ±â {
 		Scanner scanner = new Scanner(System.in);
 		
 		int a = scanner.nextInt();
+		
 		if(a >= 1 && a <= 4000) {
-			if((a%4)==0  &&(a%100)!=0 && (a%400)==0){
+			if((a%4)==0  &&(a%100)!=0 || (a%400)==0){
 				System.out.println("1");
 			}
 			else {
