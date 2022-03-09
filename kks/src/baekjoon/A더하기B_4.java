2aef9c739fe5d86e92a1bf3eeac77d85634b1d35
kyu05 <kyu05@121.139.36.131>
2022-03-09 22:22:15 +0900

    백준연습

---
+++ kks/src/baekjoon/A더하기B_4.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class A¥ı«œ±‚B_4 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		
+		
+		
+		for(int i = 1;i <= x;i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			System.out.println("Case #"+i+": " + a +" + "+ b +" = "+(a+b) );
+		}
+
+	}
+
+}
---
+++ kks/src/baekjoon/X보다작은수.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class X보다작은수 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n =scanner.nextInt();
+		int x =scanner.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			int a =scanner.nextInt();
+			
+			if(a > x) {
+				System.out.println(a);
+			}
+		}
+
+	}
+
+}
