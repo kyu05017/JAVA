7ea6e1e7d93b45058d1efb74c03d9ffa4c397089
504 <504@DESKTOP-M91NLNL>
2022-03-10 10:14:16 +0900

    메소드배우기

--- kks/src/Day07/Day07_1.java
+++ kks/src/Day07/Day07_1.java
@@ -1,10 +1,16 @@
 package Day07;
 
-public class Day07_1 {
+public class Day07_1 { // c s
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//메소드 호출하는 방법
+			// 1.메소드가 존재하는 클래스의 객체가 필요
+			
+		//1.객체 선언
+		Calculator myCalc = new Calculator();
+		//클래스명 객체명[이름] = 메모리할당 생성자();
 
-	}
+	}// m e
 
-}
+}// c e
