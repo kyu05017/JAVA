73778a3d9fe0b750386c09e510b798c140bf6d9a
504 <504@DESKTOP-M91NLNL>
2022-03-10 10:16:55 +0900

    메소드 배우기

--- kks/src/Day07/Day07_1.java
+++ kks/src/Day07/Day07_1.java
@@ -6,11 +6,20 @@ public class Day07_1 { // c s
 		
 		//메소드 호출하는 방법
 			// 1.메소드가 존재하는 클래스의 객체가 필요
+			// 2. 객체명.메소드명()
+				// EX) scanner.nextInt();
+					// .(도트) : 접근연산자 ( 필드나 메소드 호출시 )
+		
+		
 			
-		//1.객체 선언
+		// 1.객체 선언
 		Calculator myCalc = new Calculator();
 		//클래스명 객체명[이름] = 메모리할당 생성자();
-
+		
+		// 2.메소드 호출
+		myCalc.powerOn();
+		
+		
 	}// m e
 
 }// c e
