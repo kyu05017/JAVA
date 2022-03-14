a9c1751a3ec0d5cd9f21779ebec2a0ea0c785ec1
504 <504@DESKTOP-M91NLNL>
2022-03-14 09:06:44 +0900

    버프리드 연습

--- kks/src/MyPratice/Buffered연습2.java
+++ kks/src/MyPratice/Buffered연습2.java
@@ -1,23 +1,29 @@
 package MyPratice;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Buffered연습2 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-
+		//객체선언
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		//입력받음
 		int A = Integer.parseInt(br.readLine());
-		
 		String B = br.readLine();
-		
-		System.out.println(A);
-		System.out.println(B);
 
+		//출력
+		bw.write(A+"\n");
+		bw.write(B+"\n");
+		
+		//닫아줘야함
+		bw.close();
 	}
 
 }
