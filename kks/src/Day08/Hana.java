d4459c37377b17296af69af6fc5367f12a9544ad
504 <504@DESKTOP-M91NLNL>
2022-03-15 10:22:03 +0900

    10회차 문제 풀이

--- kks/src/Day08/Hana.java
+++ kks/src/Day08/Hana.java
@@ -3,9 +3,11 @@ package Day08;
 import Day09.Bank;
 
 public class Hana extends Bank{
+
 	
 	public Hana(String banknum, String bankpw, String name, int mymoney,int loanmoney) {
 		super(banknum, bankpw, name, mymoney,loanmoney);
+
 	}
 
 }
