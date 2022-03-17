a114a4e779f0e0611e9e09886a14eaa75517aaf5
504 <504@DESKTOP-M91NLNL>
2022-03-17 12:44:47 +0900

    개인과제 11회차

---
+++ kks/src/개인과제_11번째_회원제프로그램_클래스필드/Member.java
@@ -0,0 +1,10 @@
+package 개인과제_11번째_회원제프로그램_클래스필드;
+
+public class Member {
+	
+	String id; 
+	String password; 
+	String name;
+	String phone;
+	
+}
---
+++ kks/src/개인과제_11번째_회원제프로그램_클래스필드/개인과제_11번째_회원제프로그램_클래스필드.java
@@ -0,0 +1,23 @@
+package 개인과제_11번째_회원제프로그램_클래스필드;
+
+import java.util.Scanner;
+
+public class 개인과제_11번째_회원제프로그램_클래스필드 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		Member[] memberlsit = new Member[100];
+		
+		while(true) {
+			System.out.println("----------------회원제 방문록-----------------");
+			System.out.println("1.회원가입 2.로그인 3.아이디찾기 4.비밀번호 찾기 5.종료");
+			System.out.println("------------------------------------------");
+			int ch = scanner.nextInt();
+			
+			
+		}
+		
+	}
+	
+}
