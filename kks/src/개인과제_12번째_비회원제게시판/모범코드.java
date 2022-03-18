38cec5c6bf8a18cef492590edb94a0a033a04c20
504 <504@DESKTOP-M91NLNL>
2022-03-18 09:23:15 +0900

    개인과제

--- kks/src/개인과제_12번째_비회원제게시판/모범코드.java
+++ kks/src/개인과제_12번째_비회원제게시판/모범코드.java
@@ -21,19 +21,19 @@ public class 모범코드 {
 				int index = 0;
 				for(Board temp : boardlist) {
 					if(temp != null) {
-						System.out.println("출력할꺼임");
+						System.out.printf("%d\t%s\t%s \n" , (index)+1 , temp.writer , temp.title);
 					}
+					index++;
 				}
-				System.out.println("1)글작성 2)글보기 3)종료");
+				System.out.println("1)글작성 2)게시판 3)종료");
 				int ch = scanner.nextInt();
 				
 				if(ch == 1) {
 					System.out.println("글쓰기))");
 					System.out.println("제목 :");
-					scanner.next();
-					String title = scanner.nextLine();
+					String title = scanner.next();
 					System.out.println("내용 :");
-					String contents = scanner.nextLine();
+					String contents = scanner.next();
 					System.out.println("ID  :");
 					String id = scanner.next();
 					System.out.println("PW  : ");
@@ -51,7 +51,58 @@ public class 모범코드 {
 					}
 				}
 				else if(ch == 2) {
-					// 아 귀찮아
+					System.out.println("게시판))");
+					System.out.println("게시물 번호 선택");int bno = scanner.nextInt()-1;
+					System.out.printf("작성자: %s  제목: %s\n",boardlist[bno].writer,boardlist[bno].title);
+					System.out.printf("내용 : %s\n",boardlist[bno].content);
+					System.out.println("1)목록보기 2)글삭제 3)글수정"); 
+					int ch2 = scanner.nextInt();
+					if(ch2 == 1) {
+						System.out.println("목록으로 돌아갑니다.");
+					}
+					else if(ch2 == 2) {
+						System.out.println("글삭제))");
+						System.out.println("PW : ");
+						String pw = scanner.next();
+						if(boardlist[bno].password.equals(pw)) {
+							for(int i = 0; i <boardlist.length; i++) {
+								if(i == boardlist.length-1) {
+									boardlist[boardlist.length-1] = null;
+								}
+								else {
+									boardlist[i] = boardlist[i+1];
+								}
+							}
+						}
+						System.out.println("글삭제 완료");
+					}
+					else if(ch2 == 3) {
+						System.out.println("글수정))");
+						System.out.println("PW : ");
+						String pw = scanner.next();
+						if(boardlist[bno].password.equals(pw)) {
+							System.out.println("수정할 제목 : ");
+							String newtitle = scanner.next();
+							System.out.println("수정할 내용 : ");
+							String new_contents = scanner.next();
+					
+							Board board = new Board(newtitle, new_contents, boardlist[bno].writer, boardlist[bno].password);
+			
+							boardlist[bno] = board;
+							System.out.println("수정이 완료 되었습니다.");
+							
+							
+							
+							System.out.println("수정이 완료 되었습니다.");
+						}
+						else {
+							System.out.println("비밀번호가 잘묏됐습니다.");
+						}
+						
+					}
+					else {
+						System.out.println("존재하지 않는 메뉴 입니다.");
+					}
 				}
 				else if(ch == 3) {
 					System.out.println("프로그램을 종료 합니다.");
