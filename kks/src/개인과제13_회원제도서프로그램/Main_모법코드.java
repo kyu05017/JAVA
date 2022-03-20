ff5f621bd424919aec3e918c32ea7951aa3e9c02
kyu05 <kyu05@220.74.2.193>
2022-03-20 18:50:54 +0900

    개인과제

--- kks/src/개인과제13_회원제도서프로그램/Main_모법코드.java
+++ kks/src/개인과제13_회원제도서프로그램/Main_모법코드.java
@@ -1,6 +1,32 @@
 package 개인과제13_회원제도서프로그램;
 
+import java.util.Scanner;
+
 public class Main_모법코드 {
 	
+	static Scanner sc = new Scanner(System.in);
+	static Member[] members = new Member[1000];
+	static Book[] booklist = new Book[1000]; 
 	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	void Menu() {
+		
+		
+	}
+	
+	
+	void MemberMenu() {
+		
+	}
+	
+	
+	
+	void Adminmenu() {
+		
+	}
 }
