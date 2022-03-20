613e25a725b56965c27a99cee7fd4c5fa0ed57eb
504 <504@DESKTOP-0MVNHLQ>
2022-03-21 08:38:29 +0900

    백준연습

---
+++ kks/src/MyPractice04_Beakjoon/평균_배열.java
@@ -0,0 +1,28 @@
+package MyPractice04_Beakjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Æò±Õ_¹è¿­ {
+	
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		double arr[] = new double[in.nextInt()];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = in.nextDouble();
+		}
+		in.close();
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += ((arr[i] / arr[arr.length-1]) * 100);
+		}
+		System.out.print(sum / arr.length);
+		
+	}
+}
