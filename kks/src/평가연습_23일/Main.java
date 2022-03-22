b102af503c753fa4c7c5227617ea6e3073f41f48
504 <504@DESKTOP-0MVNHLQ>
2022-03-22 11:30:55 +0900

    ㅇㅇ

--- kks/src/평가연습_23일/Main.java
+++ kks/src/평가연습_23일/Main.java
@@ -1,37 +1,59 @@
 package 평가연습_23일;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		
 		Scanner scanner = new Scanner(System.in);
 		Controller con = new Controller();
 		
 		while(true) {
 			try {
-			System.out.println("--------------------주차 현황 표--------------------");
-			System.out.println("현재 시간 : ");
-			//시간 입력
+			Date date  = new Date();
+			SimpleDateFormat cardate = new SimpleDateFormat("yyyy - MM - dd HH : mm");
+			String carInTime = cardate.format(date);
 			
-			System.out.println("\t날짜\t\t차량번호\t입차시간\t출차시간\t금액");
+			System.out.println("----------------------주차 현황 표----------------------");
+			//시간 입력
+			System.out.println("\t현재 시간 : "+ carInTime);
+			System.out.println();
+			System.out.println("\t날짜\t차량번호\t입차시간\t출차시간\t금액");
 			//차량출력
 			
-			System.out.println("-------------------------------------------------");
+			for(Parking temp : Controller.carlist) {
+				String pay_zero = null;
+				if(temp != null) {
+					if(temp.getisCar_state()) {
+						pay_zero = "정산 전";
+					}
+					else {
+						DecimalFormat df = new DecimalFormat("#,##0원");
+						pay_zero = df.format(temp.getCar_PayMoney());
+					}
+					System.out.printf("%s\t%s\t%s\t%s\t%s\n",temp.getCar_ParkingDate(),temp.getCar_number(),temp.getCar_ParkingTime(),temp.getCar_OutTime(),pay_zero);
+				}
+			}
+			
+			System.out.println("-----------------------------------------------------");
 			System.out.println("1.입차 2.출차");
 			
-			String ch = scanner.next();
+			String ch = scanner.nextLine();
 			
 			if(ch.equals("1") || ch.equals("입차")) {
-				System.out.println("차량번호 입력");
-				String inParking_Car_Number = scanner.next();
-				
+				System.out.println("차량번호 입력 : ");
+				String inParking_Car_Number = scanner.nextLine();
 				con.in_Parking(inParking_Car_Number);
+				
 			}
 			else if(ch.equals("2") || ch.equals("출차")) {
-				System.out.println("차량번호 입력");
-				String outParking_Car_Number = scanner.next();
+				System.out.println("차량번호 입력 : ");
+				String outParking_Car_Number = scanner.nextLine();
 				
 				con.out_Oarking(outParking_Car_Number);
 			}
@@ -43,7 +65,7 @@ public class Main {
 			System.out.println("관리자 문의 요망");
 			scanner = new Scanner(System.in);
 			}
-		
 		}
+		
 	}
 }
--- kks/src/평가연습_23일/Parking.java
+++ kks/src/평가연습_23일/Parking.java
@@ -3,18 +3,45 @@ package 평가연습_23일;
 public class Parking {
 	
 	private String car_number;
+	private String car_ParkingDate;
 	private String car_ParkingTime;
 	private String car_OutTime;
+	private boolean car_state;
 	private int car_PayMoney;
 	
 	public Parking() {
 	}
 	
-	public Parking(String car_number, String car_ParkingTime, String car_OutTime, int car_PayMoney) {
+	public Parking(String car_number, String car_ParkingDate, String car_ParkingTime, String car_OutTime,
+			boolean car_state, int car_PayMoney) {
 		this.car_number = car_number;
+		this.car_ParkingDate = car_ParkingDate;
 		this.car_ParkingTime = car_ParkingTime;
 		this.car_OutTime = car_OutTime;
+		this.car_state = car_state;
 		this.car_PayMoney = car_PayMoney;
+	}
+
+
+
+
+
+
+
+	public String getCar_ParkingDate() {
+		return car_ParkingDate;
+	}
+
+	public void setCar_ParkingDate(String car_ParkingDate) {
+		this.car_ParkingDate = car_ParkingDate;
+	}
+
+	public boolean getisCar_state() {
+		return car_state;
+	}
+
+	public void setCar_state(boolean car_state) {
+		this.car_state = car_state;
 	}
 
 	public String getCar_number() {
