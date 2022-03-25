081fd0d9f5e561e75bc29a05e84e4b6dd152f554
504 <504@DESKTOP-0MVNHLQ>
2022-03-25 11:44:05 +0900

    18일차

--- kks/src/Day18/Day18_3.java
+++ kks/src/Day18/Day18_3.java
@@ -1,5 +1,9 @@
 package Day18;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Day18_3 {
 	
 	public static void main(String[] args) {
@@ -16,5 +20,38 @@ public class Day18_3 {
 		// 검색 			빠름						
 		// 삭제 									빠름
 		
+		
+		// p. 732
+		
+		// 1. 어레이 리스트 객체 선언 [ 제네릭 : 문자열 ]
+		ArrayList<String> list1 = new ArrayList<>();
+		// 2. 링크드 리스트 객체 선언 [ 제네릭 : 문자열 ]
+		LinkedList<String> list2 = new LinkedList<>();
+		
+		// 3. 시간 변수
+		long statTime;
+		long endTime;
+		
+		// ArrayList 10000개의 객체를 삽입하는데 걸리는 시간 체크
+		statTime = System.nanoTime();
+		for(int i = 0 ; i < 10000; i++) {
+		//	list1.add(0, i+""); 정수형을 문자열로 변환 
+			list1.add(0,String.valueOf(i)); // 정수형을 문자열로 변환 
+			// 문자열을 정수로 바꾸는 법
+			// Integer.ParseInt(문자열);
+		}
+		endTime = System.nanoTime();
+		System.out.println("ArrayList 걸린 시간 : "+ (endTime - statTime) + "ns");
+		
+		// LinkedList 10000개의 객체를 삽입하는데 걸리는 시간 체크
+		statTime = System.nanoTime();
+		for(int i = 0 ; i < 10000; i++) {
+			//	list2.add(0, i+""); 정수형을 문자열로 변환 
+				list2.add(0,String.valueOf(i)); // 정수형을 문자열로 변환 
+				// 문자열을 정수로 바꾸는 법
+				// Integer.ParseInt(문자열);
+		}
+		endTime = System.nanoTime();
+		System.out.println("LinkedList 걸린 시간 : "+ (endTime - statTime) + "ns");
 	}
 }
