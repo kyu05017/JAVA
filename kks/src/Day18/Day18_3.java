3eeb5385ffc612ea9fff390ff0aec7235592ea51
504 <504@DESKTOP-0MVNHLQ>
2022-03-25 11:31:28 +0900

    18일차

---
+++ kks/src/Day18/Day18_3.java
@@ -0,0 +1,20 @@
+package Day18;
+
+public class Day18_3 {
+	
+	public static void main(String[] args) {
+	
+		// p. 731
+			// LinkedList 클래스
+		
+		// 			ArrayList	 VS		 LinkedList 
+		// 			   배열                   링크
+		//내부구조차이   인덱스 기준 메모리연결     서로다른 메모리 주소 연결
+		//   속도    
+		//add[ 추가 ]    빠름                  
+		//add[ 삽입 = 중간]                       빠름
+		// 검색 			빠름						
+		// 삭제 									빠름
+		
+	}
+}
