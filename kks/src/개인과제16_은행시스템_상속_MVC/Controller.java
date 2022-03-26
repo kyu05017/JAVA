b0bc48b90981e3009ccae236cb91bac4c2ebe13a
kyu05 <kyu05@121.139.36.110>
2022-03-26 21:44:34 +0900

    개인과제 16 상속 MVC

---
+++ kks/src/개인과제16_은행시스템_상속_MVC/Controller.java
@@ -0,0 +1,375 @@
+package 개인과제16_은행시스템_상속_MVC;
+
+import java.util.Scanner;
+
+import 모바일_뱅크_시스템.Bankbook;
+import 모바일_뱅크_시스템.Day08_5;
+import 모바일_뱅크_시스템.Loan;
+
+public class Controller {
+	
+	static Account[] accountlist = new Account[1000];
+	static Bank_Book[] banklist = new Bank_Book[1000];
+	static Loan_System[] loanlist = new Loan_System[1000];
+	
+/////////////////////////////////////////////////////////////////////////////////////////
+	
+	boolean registration(String id,String pw,String name,String phone) {
+		
+		Account account = new Account(name, id, pw, phone);
+		int i = 0;
+		for(Account temp : accountlist) {
+			if(temp == null) {
+				accountlist[i] = account;
+				return true;
+			}
+			i++;
+		}
+		return false;
+	}
+	
+	String login(String id, String pw) {
+		
+		for(Account temp : accountlist) {
+			
+			if(temp != null && id.equals("admin") && temp.getPw().equals(pw)) {
+				return "admin";
+			}
+			else if(temp != null && temp.getId().equals(id) && temp.getPw().equals(pw)) {
+				return temp.getId();
+			}
+		}
+		return "실패";
+	}
+	
+	String findid(String name, String phone) {
+		
+		for(Account temp : accountlist) {
+			
+			if(temp != null && temp.getName().equals(name) && temp.getPhone().equals(phone)) {
+				return temp.getId();
+			}
+		}
+		return "실패";
+	}
+	
+	String findpw(String id, String phone) {
+		
+		for(Account temp : accountlist) {
+
+			if(temp != null && temp.getId().equals(id) && temp.getPhone().equals(phone)) {
+				return temp.getPw();
+			}
+		}
+		return "실패";
+	}
+	
+/////////////////////////////////////////////////////////////////////////////////////////
+	
+	boolean bankregistration(String id, int bnum, String pw) {
+		
+		Bank_Book bank = new Bank_Book(bnum, 0, pw, id, 0);
+		
+		int i = 0;
+		for(Bank_Book temp : banklist) {
+			if(temp == null) {
+				banklist[i] = bank;
+				return true;
+			}
+			i++;
+		}
+		
+		return false;
+	}
+	
+	boolean inmoney(String id, int bnum, String pw, int money) {
+		
+		int i = 0;
+		for(Bank_Book temp : banklist) {
+			if(temp != null &&temp.getBanknum() == bnum && temp.getBankpw().equals(pw) && temp.getUsername().equals(id)) {
+				int a = banklist[i].getNowmoney() + money;
+				banklist[i].setNowmoney(a);
+				return true;
+			}
+			i++;
+		}
+
+		return false;
+	}
+	
+	boolean outmoney(String id, int bnum, String pw, int money) {
+		int i = 0;
+		for(Bank_Book temp : banklist) {
+			if(temp != null &&temp.getBanknum() == bnum && temp.getBankpw().equals(pw) && temp.getUsername().equals(id)) {
+				int a = banklist[i].getNowmoney() - money;
+				banklist[i].setNowmoney(a);
+				return true;
+			}
+			i++;
+		}
+
+		return false;
+	}
+	
+	void bankbooklsit(String id) {
+		
+		for(Bank_Book temp : banklist) {
+			if(temp != null && temp.getUsername().equals(id)) {
+				System.out.println(temp.getBanknum() + "\t"+ temp.getNowmoney()+"\t"+temp.getLoanm());
+			}
+		}
+	}
+	
+	void sendmoney(String id, int bnum, String pw) { 
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int i = 0;
+		for(Bank_Book temp : banklist) {
+			if(temp != null && temp.getBanknum() == bnum && temp.getBankpw().equals(pw)) {
+				System.out.print("받으실분 계좌번호  : "); 
+				int bnum2 = scanner.nextInt();
+				int j = 0;
+				for(Bank_Book temp2 : banklist) {
+					if(temp2 != null && temp2.getBanknum() == bnum2) {
+						System.out.print("이체할 금액을 입력하세요. : ");
+						int money2 = scanner.nextInt();
+						if(temp.getNowmoney() < money2) {
+							System.out.println("알림)) 잔액이 부족하여 이체를 실패했습니다.");
+						}
+						else {
+							int a = banklist[j].getNowmoney() + money2;
+							int b = banklist[i].getNowmoney() - money2;
+							banklist[j].setNowmoney(a);
+							banklist[i].setNowmoney(b) ;
+							System.out.println("계좌번호 "+bnum+"에 "+money2+"원을 이체 했습니다.");
+							return;
+						}
+					}
+					j++;
+				}
+			}
+			i++;
+		}
+		
+	}
+	
+	void findbankpw(String id, int bnum, String phone) {
+		
+		for(Bank_Book temp : banklist) {
+			if(temp != null && temp.getBanknum() == bnum && temp.getUsername().equals(id)) {
+				for(Account temp2 : accountlist) {
+					if(temp2 != null && temp2.getPhone().equals(phone)) {
+						System.out.println("회원님의계좌 비밀번호는 "+ temp.getBankpw()+ "입니다");
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+/////////////////////////////////////////////////////////////////////////////////////////	
+	
+	void newloan(String lname, int loan) {
+		
+		for(Loan_System temp : loanlist) {
+			if(temp!=null && temp.getLname().equals(lname)) {
+				System.out.println("알림)) 현재 사용중인 대출 상품입니다.[등록실패]");
+				return; 
+			}
+		}
+		
+		Loan_System newloan = new Loan_System(lname, loan, " ", false);
+		
+		int i = 0;
+		for(Loan_System temp : loanlist) {
+			if(temp==null) {
+				System.out.println("알림)) 대출 상품 등록이 완료되었습니다.");
+				loanlist[i]=newloan;
+				return;
+			}
+			i++;
+		}
+	}
+	
+	void loanlsit(String id) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(Loan_System temp : loanlist) {
+			if(temp!=null) {
+				System.out.println(temp.getLname()+"\t"+temp.getLoanmoney()+"\t"+(temp.getInterest()*100)+"%"+temp.getLid());
+			}
+		} 
+		System.out.println("1)대출받기  2)뒤로가기");
+		int ch = scanner.nextInt();
+		
+		
+		if(ch == 1) {
+			System.out.println("알림)) 대출 페이지로 이동합니다.");
+			inloan(id);
+		}
+		else if(ch == 2) {
+			System.out.println("알림)) 이전으로 돌아갑니다.");
+			return;
+		}
+	}
+	
+	void removeloan() {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(Loan_System temp : loanlist) {
+			if(temp!=null) {
+				System.out.println(temp.getLname()+"\t"+temp.getLoanmoney()+"\t"+(temp.getInterest()*100)+"%"+temp.getLid());
+			}
+		}
+		String lname =scanner.next();	
+		int a =0;
+		for(Loan_System temp : loanlist) {
+			if(temp!=null && !temp.getLname().equals(lname)) {
+				
+			}
+			else if(temp!=null && temp.getLname().equals(lname)) {
+				System.out.println("검색 결과 : ");
+				System.out.println("상품이름\t대출금액\t이자율");
+				System.out.println(temp.getLname()+"\t"+temp.getLoanmoney()+"\t"+(temp.getInterest()*100)+"%");
+				System.out.println("1.삭제 2.취소"); int ch = scanner.nextInt();
+				
+				if(ch==1) {
+					System.out.println("알림)) 해당 상품이 삭제되었습니다.");
+					loanlist[a]=null;
+					int j = 0;	
+					for(Loan_System temp2 : loanlist) {
+						if(temp2==null) {
+							for(int i=j; i<loanlist.length; i++) {
+								if(i==loanlist.length-1) {
+									loanlist[loanlist.length-1] = null;
+								}
+								else {
+									loanlist[i]=loanlist[i+1];
+								}
+							}
+						}
+						j++;	
+					} // for end
+					return;
+				} // if end
+				else if(ch==2) {
+					System.out.println("알림)) 관리자 메뉴로 돌아갑니다.");
+					return;
+				}
+				else System.out.println("알림)) 알 수 없는 입력입니다.");
+					return;
+			}
+			a++;
+		} // for end
+		System.out.println("알림)) 일치하는 대출 상품이 없습니다.");
+	}
+	
+	void myloan(String id) {
+		Scanner scanner = new Scanner(System.in);
+		
+		for(Loan_System temp : loanlist) {
+			if(temp != null &&temp.getLid().equals(id)) {
+				System.out.println(temp.getLname() +" "+ (temp.getLoanmoney()+(temp.getLoanmoney()*temp.getInterest())));
+				System.out.println("1)대출상환  2)뒤로가기");
+				String ch = scanner.next();
+				if(ch.equals("1")) {
+					System.out.println("알림)) 대출상환 페이지로 이동합니다.");
+					outloan(id);
+				}
+				else if(ch.equals("2")) {
+					System.out.println("알림)) 이전으로 돌아갑니다.");
+					return;
+				}
+				
+			}
+		}
+	}
+	
+	void outloan(String id) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("대출상환)) ");
+		for(Loan_System temp : loanlist) {
+			if(temp!=null && id.equals(temp.getLid())) {
+				System.out.println(temp.getLname()+"\t"+temp.getLoanmoney()+"\t"+(temp.getInterest()*100)+"%\t"+temp.getLid());
+			}
+		}
+		System.out.print("상환할 대출상품의 이름을 입력하세요 : \n");
+		String out = scanner.next();
+		try {
+			int j = 0;
+			for(Loan_System temp : loanlist) {
+				
+				if(temp!=null && !temp.getLname().equals(out)) {}
+				else if(temp != null && temp.getLname().equals(out)) {
+					System.out.print("출금할 계좌번호를 입력하세요.  : \n");
+					int bnum = scanner.nextInt();
+					int i = 0;
+					for(Bank_Book temp2 : banklist) {
+						if(temp2 != null && temp2.getNowmoney() < (temp.getLoanmoney()+(temp.getLoanmoney()*temp.getInterest()))) {
+							System.out.println("알림)) 잔액이 부족하여 상환하실수 없습니다.");
+							return;
+						}
+						else if(temp2 != null && temp2.getUsername().equals(id) && temp2.getBanknum() == bnum) {
+							System.out.println(temp.getLname()+" 상품을 상환 합니다.");
+							System.out.println("상환 금액은 "+ (temp.getLoanmoney()+(temp.getLoanmoney()*temp.getInterest())) + "원 입니다.");
+							int a = (int) (banklist[i].getNowmoney() - (temp.getLoanmoney()+(temp.getLoanmoney()*temp.getInterest())));
+							banklist[i].setNowmoney(a);
+							banklist[i].setLoanm(0);
+							loanlist[j].setLid(" ");
+							System.out.println("알림)) 상환이 완료되었습니다. 이용해주셔서 감사합니다.");
+							return;
+						}
+						i++;
+					}
+				} 
+				
+				j++;
+			}
+			System.out.println("알림)) 존재하지 않는 상품입니다.");
+		}
+		catch(NullPointerException e) {
+		
+		}
+	}
+	
+	void inloan(String id) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("대출받기)) ");
+		System.out.print("받으실 대출상품의 이름을 입력하세요 : \n");
+		String ln = scanner.next();
+		try {
+			int j = 0;
+			for(Loan_System temp : loanlist) {
+				if(temp != null && temp.getLname().equals(ln)) {
+					System.out.print("받으실 계좌번호를 입력하세요.  : \n");
+					int bnum = scanner.nextInt();
+					int i = 0;
+					for(Bank_Book temp2 : banklist) {
+						if(temp2 != null && temp2.getUsername().equals(id)&&temp2.getBanknum() == bnum) {
+							System.out.println(temp.getLname()+" 상품을  대출받았습니다.");
+							int a = banklist[i].getNowmoney() + temp.getLoanmoney();
+							int b = (int) (temp.getLoanmoney() + (temp.getLoanmoney()*temp.getInterest()));
+							banklist[i].setNowmoney(a);
+							banklist[i].setLoanm(b);
+							loanlist[j].setLid(id);
+							return;
+						}
+						i++;
+					}
+				}
+				
+				j++;
+			}
+			System.out.println("알림)) 존재하지 않는 상품입니다.");
+		}
+		catch(NullPointerException e) {
+			System.out.println("알림)) 잘못된 정보입니다.");
+		}
+	}
+}
---
+++ kks/src/개인과제16_은행시스템_상속_MVC/Main.java
@@ -0,0 +1,296 @@
+package 개인과제16_은행시스템_상속_MVC;
+
+import java.util.Scanner;
+
+import 모바일_뱅크_시스템.Day08_5;
+
+public class Main {
+	
+	static Controller con = new Controller();
+	
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		main.menu();
+	}
+	
+	void menu() {
+		
+		Scanner scanner = new Scanner(System.in);
+		Main main = new Main();
+		
+		while(true) { // 무한루프 [종료조건 x]
+			System.out.println("-------------모바일 뱅크 프로그램-------------------");
+			System.out.println("1.회원가입 2.로그인 3.아이디찾기 4.비밀번호찾기 5.종료");
+			
+			String ch = scanner.next();
+			
+			if(ch.equals("1")) {
+				System.out.println("회원가입)");
+				System.out.print("ID : \n");
+				String id = scanner.next();
+				for(Account temp : Controller.accountlist) {
+					if(temp != null && temp.getId().equals(id)) {
+						System.out.println("알림)) 현재 사용중인 아이디입니다.");
+						break;
+					}
+				}
+				System.out.print("PW : \n");
+				String pw = scanner.next();
+				System.out.print("Name : \n");
+				String name = scanner.next();
+				System.out.print("Phone : \n");
+				String phone = scanner.next();
+				
+				boolean result = con.registration(id,pw,name,phone);
+				
+				if(result) {
+					System.out.println("회원가입이 완료 되었습니다.");
+				}
+				else {
+					System.out.println("회원가입에 실패했습니다.");
+				}
+
+			}
+			else if(ch.equals("2")) {
+				System.out.println("로그인)");
+				System.out.print("ID : \n");
+				String id = scanner.next();
+				System.out.print("PW : \n");
+				String pw = scanner.next();
+				
+				String result = con.login(id,pw);
+				
+				if(result.equals("admin")) {
+					System.out.println("관리자로 로그인 했습니다");
+					main.adminmenu();
+				}
+				else if(result.equals(id)) {
+					System.out.println(id+"님 환영합니다.");
+					main.membermenu(result);
+				}
+				else if(result.equals("실패")) {
+					System.out.println("로그인에 실패 했습니다.");
+				}
+				
+			}
+			else if(ch.equals("3")) {
+				System.out.println("아이디 찾기))");
+				System.out.print("Name : \n");
+				String name = scanner.next();
+				System.out.print("Phone : \n");
+				String phone = scanner.next();
+				
+				String result = con.findid(name,phone);
+				
+				if(result.equals("실패")) {
+					System.out.println("아이디 찾기에 실패 했습니다.");
+				}
+				else {
+					System.out.println("회원님의 아이디는 " + result + "입니다.");
+				}
+				
+			}
+			else if(ch.equals("4")) {
+				System.out.println("비밀번호 찾기)");
+				System.out.print("ID : \n");
+				String id = scanner.next();
+				System.out.print("Phone : \n");
+				String phone = scanner.next();
+				
+				String result = con.findpw(id,phone);
+				
+				if(result.equals("실패")) {
+					System.out.println("비밀번호 찾기에 실패 했습니다.");
+				}
+				else {
+					System.out.println("회원님의 비밀번호는 " + result + "입니다.");
+				}
+			}
+			else if(ch.equals("5")) {
+				System.out.println("프로그램 종료.");
+				return;
+			}
+			else {
+				System.out.println("알림)) 잘못된 입력입니다.");
+			}
+		}
+	}
+	
+	
+	void membermenu(String id) {
+		
+		Scanner scanner = new Scanner(System.in);
+		Main main = new Main();
+		
+		while(true) {
+			System.out.println("------------------회원 메뉴---------------");
+			System.out.println("1.계좌생성 2.입금 3.출금 4.계좌목록 5.이체 6.대출 7.계좌비밀번호찾기 8.로그아웃");
+			String ch = scanner.next();
+			
+			if(ch.equals("1")) {
+				System.out.println("계좌가입) ");
+				System.out.print("사용할 계좌번호 입력 : \n");
+				int banknum = scanner.nextInt();
+				
+				for(Bank_Book temp : Controller.banklist) {
+					if(temp != null && temp.getBanknum() == banknum) {
+						System.out.println("알림)) 현재 사용중인 계좌번호입니다.");
+						break;
+					}
+				}
+				System.out.print("사용할 비민번호 입력 : \n");
+				String pw = scanner.next();
+				
+				
+				boolean result =  con.bankregistration(id,banknum,pw);
+				
+				if(result) {
+					System.out.println("계좌생성이 완료 되었습니다.");
+				}
+				else {
+					System.out.println("계좌 생성 실패");
+				}
+			}
+			else if(ch.equals("2")) {
+				System.out.print("계좌번호  입력 	: \n");
+				int banknum = scanner.nextInt();
+				System.out.print("계좌 비밀번호 입력 : \n");
+				String pw = scanner.next();
+				System.out.print("입금할 금액입력 	: \n");
+				int money = scanner.nextInt();
+				
+				boolean result = con.inmoney(id,banknum,pw,money);
+				
+				if(result) {
+					System.out.println("알림)) "+money + "원이 계좌로 입금 되었습니다");
+				}
+				else {
+					System.out.println("입금 실패");
+				}
+				
+			}
+			else if(ch.equals("3")) {
+				System.out.print("계좌번호  입력 	: \n");
+				int banknum = scanner.nextInt();
+				System.out.print("계좌 비밀번호 입력 : \n");
+				String pw = scanner.next();
+				System.out.print("출금할 금액입력	: \n");
+				int money = scanner.nextInt();
+				
+				boolean result = con.outmoney(id,banknum,pw,money);
+				
+				if(result) {
+					System.out.println("알림)) "+money + "원이 계좌에서 출금 되었습니다");
+				}
+				else {
+					System.out.println("출금 실패");
+				}
+			}
+			else if(ch.equals("4")) {
+				System.out.println("계좌 목록))");
+				System.out.print("계좌번호\t현재잔액\t대출받은금액\n");
+				
+				con.bankbooklsit(id);
+			}
+			else if(ch.equals("5")) {
+				System.out.println("이체))");
+				System.out.print("계좌 번호 입력 	: "); 
+				int bname = scanner.nextInt();
+				System.out.print("계좌 비밀번호 입력 	: "); 
+				String pw = scanner.next();
+				
+				con.sendmoney(id,bname,pw);
+			}
+			else if(ch.equals("6")) {
+				
+				main.loanmenu(id);
+			}
+			else if(ch.equals("7")) {
+				System.out.println("비밀번호 찾기))");
+				System.out.print("계좌번호 	: \n"); 
+				int bnum = scanner.nextInt();
+				System.out.print("연락처 		: \n"); 
+				String phone = scanner.next();
+				
+				con.findbankpw(id,bnum,phone);
+			}
+			else if(ch.equals("8")) {
+				System.out.println("로그아웃 했습니다.");
+				return;
+			}
+			else {
+				System.out.println("알림)) 잘못된 입력입니다.");
+			}
+		}
+	}
+	
+	void adminmenu() {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("------------------관리자 메뉴---------------");
+			System.out.println("1.대출상품등록 2.대출상품삭제 3.로그아웃");
+			String ch = scanner.next();
+			
+			if(ch.equals("1")) {
+				System.out.println("대출 상품 등록 페이지))");
+				// 1. 입력받기
+				System.out.print("대출 상품이름 입력 : \n"); 
+				String lname = scanner.next();
+				System.out.print("대출 금액 입력 : \n"); 
+				int loan = scanner.nextInt();
+				
+				con.newloan(lname,loan);
+				
+			}
+			else if(ch.equals("2")) {
+				System.out.println("대출 상품 삭제 페이지))");
+				System.out.print("삭제할 대출 상품이름 입력 : \n"); 
+				
+				
+				con.removeloan();
+			}
+			else if(ch.equals("3")) {
+				System.out.println("로그아웃 하셨습니다.");
+				return;
+			}
+			else {
+				System.out.println("알림)) 잘못된 입력입니다.");
+			}
+		} // while end
+	}
+	
+	void loanmenu(String id) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("------------------회원 메뉴---------------");
+			System.out.println("1.대출상품목록 2.대출확인 3.뒤로가기");
+			String ch = scanner.next();
+			
+			if(ch.equals("1")) {
+				System.out.println("대출상품 목록 목록))");
+				System.out.println("상품이름\t대출금액\t이자율\t대출자");
+				
+				con.loanlsit(id);
+				
+			}
+			else if(ch.equals("2")) {
+				System.out.println("대출받은상품 목록 목록))");
+				System.out.print("대출상품\t대출잔액\t\n");
+				
+				con.myloan(id);
+			}
+			else if(ch.equals("3")) {
+				System.out.println("이전 메뉴로 돌아갑니다.");
+				return;
+			}
+			else {
+				System.out.println("알림)) 잘못된 입력입니다.");
+			}
+		}
+	}
+}
