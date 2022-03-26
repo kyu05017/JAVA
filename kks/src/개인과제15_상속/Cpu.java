45c914e2e5789e26d46dc7273116ae2d6b0c29db
kyu05 <kyu05@121.139.36.110>
2022-03-26 21:33:56 +0900

    개인과제 15 상속응용

--- kks/src/개인과제15_상속/Cpu.java
+++ kks/src/개인과제15_상속/Cpu.java
@@ -3,38 +3,46 @@ package 개인과제15_상속;
 import java.util.Scanner;
 
 public class Cpu {
+	// 필드 = 온도
+	public int temperature;
 	
-	public int temperature = 0;
-	
+	//생성자 - 깡통
 	public Cpu() {
 		
 	}
 
+	//생성자 - 풀생성자
 	public Cpu(int temperature) {
 		this.temperature = temperature;
 	}
 	
+	//메소드 인수 o 반환 o 예외처리
 	public boolean cpuRun(int work) throws InterruptedException {
 		
 		System.out.println("Cpu 정상 작동중 1)실행 5)다른작업 [냉각성능 0]");
 		Scanner scanner = new Scanner(System.in);
 		String ch = scanner.next();
+		// 무한 반복 - 종료 조건 cpu 고장
 		while(true) {
+			// 온도가 100이 되면 고장 반복문 종료
 			if(temperature > 100) {
 				System.out.println("Cpu 고장");
 				return false;
 			}
 			if(work == 1) {
+				// 1초마다 5씩 증가
 				temperature += 5;
 				Thread.sleep(1000);
 				System.out.println("cpu 온도상승 +5도");
 			}
 			else if(work == 2) {
+				// 1초마다 5씩 증가
 				temperature += 5;
 				Thread.sleep(1000);
 				System.out.println("cpu 온도상승 +5도");
 			}
 			else if(work == 3) {
+				// 1초마다 20씩 증가
 				temperature += 20;
 				Thread.sleep(1000);
 				System.out.println("cpu 온도상승 +20도");
@@ -43,7 +51,7 @@ public class Cpu {
 				System.out.println("바탕화면으로 돌아갑니다.");
 				return true;
 			}
-			//temperature -= 10;
+			// 1초간 쓰레드 멈춤
 			Thread.sleep(1000);
 		}
 	}
--- kks/src/개인과제15_상속/Main.java
+++ kks/src/개인과제15_상속/Main.java
@@ -6,18 +6,20 @@ public class Main {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
+		// cpu 객체 생성
 		Cpu myCpu = new Cpu();
-		
+		// 입력객체 생성
 		Scanner scanner = new Scanner(System.in);
-		
+		// 무한반복 [ 종료 조건 메뉴외 입력 ]
 		while(true) {
 			System.out.println("컴퓨터 사용중");
 			System.out.println("1)인터넷 서핑 2)문서작업 3)게임");
 			String work = scanner.next();
 			
 			if(work.equals("1") || work.equals("인터넷")) {
+				// cpu클래스에 1을 인수로 보내고 변환값을 boolean
 				boolean result = myCpu.cpuRun(1);
-				
+				// 결과값이 거짓 이라면 상속된 new cpu로 교채
 				if(result == false) {
 					System.out.println("Cpu를 교체합니다.");
 					myCpu = new newCpu();
@@ -25,7 +27,9 @@ public class Main {
 				}
 			}
 			else if(work.equals("2") || work.equals("문서")) {
+				// cpu클래스에 2을 인수로 보내고 변환값을 boolean
 				boolean result = myCpu.cpuRun(2);
+				// 결과값이 거짓 이라면 상속된 new cpu로 교채
 				if(result == false) {
 					System.out.println("Cpu를 교체합니다.");
 					myCpu = new newCpu();
@@ -33,7 +37,9 @@ public class Main {
 				}
 			}
 			else if(work.equals("3") || work.equals("게임")) {
+				// cpu클래스에 3을 인수로 보내고 변환값을 boolean
 				boolean result = myCpu.cpuRun(3);
+				// 결과값이 거짓 이라면 상속된 new cpu로 교채
 				if(result == false) {
 					System.out.println("Cpu를 교체합니다.");
 					myCpu = new newCpu();
