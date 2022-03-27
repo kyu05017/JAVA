5e1ef741b4fdc5740cf9f10d15d8daa72f4089d7
504 <504@DESKTOP-0MVNHLQ>
2022-03-28 08:22:12 +0900

    17일차

---
+++ kks/src/Day17/Pair.java
@@ -0,0 +1,26 @@
+package Day17;
+
+public class Pair< K , V> {
+	
+	// ÇÊµå
+	private K key;
+	private V value;
+	// »ý¼ºÀÚ
+	public Pair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+	// ¸Þ¼Òµå [ get , set ] 
+	public K getKey() {
+		return key;
+	}
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public V getValue() {
+		return value;
+	}
+	public void setValue(V value) {
+		this.value = value;
+	}
+}
