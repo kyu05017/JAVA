b613fa5f71052c0fbd8700b0955168af39c17bc9
kyu05 <kyu05@121.139.36.110>
2022-03-27 10:25:31 +0900

    개인과제 16 은행 시스템 MVC 상속

---
+++ kks/src/개인과제16_은행시스템_상속_MVC/Hana.java
@@ -0,0 +1,11 @@
+package 개인과제16_은행시스템_상속_MVC;
+
+public class Hana extends Bank{
+
+	
+	public Hana(String banknum, String bankpw, String name, int mymoney,int loanmoney) {
+		super(banknum, bankpw, name, mymoney,loanmoney);
+
+	}
+
+}
---
+++ kks/src/개인과제16_은행시스템_상속_MVC/Shinhan.java
@@ -0,0 +1,8 @@
+package 개인과제16_은행시스템_상속_MVC;
+
+public class Shinhan extends Bank{
+
+	public Shinhan(String banknum, String bankpw, String name, int mymoney, int loanmoney) {
+		super(banknum, bankpw, name, mymoney, loanmoney);
+	}
+}
