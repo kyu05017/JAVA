656084db55b721d513d25b322b9ffa522d2af30f
504 <504@DESKTOP-0MVNHLQ>
2022-04-06 09:16:23 +0900

    개인과제

--- kks/src/개인과제/_27_스레드응용프로그램/Book.java
+++ kks/src/개인과제/_27_스레드응용프로그램/Book.java
@@ -3,4 +3,28 @@ package 개인과제._27_스레드응용프로그램;
 public class Book {
 	private String name;
 	private String contents;
+	
+	public Book() {}
+
+	public Book(String name, String contents) {
+		this.name = name;
+		this.contents = contents;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
 }
--- kks/src/개인과제/_27_스레드응용프로그램/control.java
+++ kks/src/개인과제/_27_스레드응용프로그램/control.java
@@ -1,4 +1,5 @@
 package 개인과제._27_스레드응용프로그램;
 
 public class control {
+	
 }
