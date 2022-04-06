c50f1d7ad34c79f8f89d946052008813281170e2
504 <504@DESKTOP-0MVNHLQ>
2022-04-06 09:36:02 +0900

    개인과제

--- kks/src/개인과제/_27_스레드응용프로그램/Control.java
+++ kks/src/개인과제/_27_스레드응용프로그램/Control.java
@@ -8,7 +8,39 @@ public class Control {
 	static Scanner scanner = new Scanner(System.in);
 	
 	
-	public static void run() {
-		
+	public static void song1() {
+		System.out.println("가사");
+		String work = scanner.next();
+		if (work.equals("e")) {
+			System.out.println("메세지)) 음악을 종료합니다.");
+		}
+		else {
+			System.out.println("메세");
+		}
 	}
+	
+	public static void song2() {
+		System.out.println("가사");
+		String work = scanner.next();
+		if (work.equals("e")) {
+			System.out.println("메세지)) 음악을 종료합니다.");
+		}
+	}
+
+	public static void song3() {
+		System.out.println("가사");
+		String work = scanner.next();
+		if (work.equals("e")) {
+			System.out.println("메세지)) 음악을 종료합니다.");
+		}
+	}
+
+	public static void song4() {
+		System.out.println("가사");
+		String work = scanner.next();
+		if (work.equals("e")) {
+			System.out.println("메세지)) 음악을 종료합니다.");
+		}
+	}
+
 }
--- kks/src/개인과제/_27_스레드응용프로그램/Main.java
+++ kks/src/개인과제/_27_스레드응용프로그램/Main.java
@@ -6,13 +6,41 @@ public class Main {
 		
 		while(true) {
 			
-			System.out.println("책 읽어주기 프로그램");
+			System.out.println("가사 리딩 프로그램");
 			
 			System.out.println("1) 실행 2) 종료");
 			String work = Control.scanner.next();
 			
 			if(work.equals("1")) {
+				System.out.println("1. 아이유 Blueming");
+				System.out.println("2. ");
+				System.out.println("3.");
+				System.out.println("4.");
 				
+				try {
+					int ch = Control.scanner.nextInt();
+					if(ch > 4) {
+						System.out.println("메세지)) 존재하지 않는 곡입니다.");
+					}
+					else {
+						System.out.println("메세지)) "+ch + "번 곡을 재생합니다.");
+						if(ch == 1) {
+							Control.song1();
+						}
+						if(ch == 2) {
+							
+						}
+						if(ch == 3) {
+							
+						}
+						if(ch == 4) {
+							
+						}
+					}
+				}
+				catch(Exception e) {
+					System.out.println("메세지)) 숫자 만 입력 가능합니다.");
+				}
 			}
 			else if(work.equals("2")) {
 				System.out.println("종료합니다.");
